cf3dcf53e6516f27f683901766217e03bd06169e
RobotoSWAG <74079951+RobotoSWAG@users.noreply.github.com>
2021-09-24 11:10:17 -0400

    Educandosa Codigo Fuente

---
+++ EDUCANDOSA/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Provedoor provedoor = new Provedoor(1209374, "binees lmao");
+        System.out.println(provedoor);
+    }
+}
